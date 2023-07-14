e184370a1e88e7e2c94aa0e576daa57c5b457684
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 17:39:32 -0500

    Create m_to_km.js

---
+++ m_to_km.js
@@ -0,0 +1,7 @@
+function convertMtoKM(meters) {
+    return meters/1000;
+}
+
+// Sample usage - do not modify
+console.log(convertMtoKM(1000)) // expected 1
+console.log(convertMtoKM(3000)) // expected 3
