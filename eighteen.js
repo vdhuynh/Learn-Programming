f3c047c5f11a5268e0096ac4c49b996ffc24c46b
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 14:52:44 -0500

    Create eighteen.js

---
+++ eighteen.js
@@ -0,0 +1,8 @@
+function isEighteenYearsOld(age) {
+    return age === 18;
+}
+
+// Sample usage (do not modify)
+console.log(isEighteenYearsOld(10))
+console.log(isEighteenYearsOld(18))
+console.log(isEighteenYearsOld(25))
