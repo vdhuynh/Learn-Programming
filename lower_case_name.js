deed1c19e53ef93e6fa0bc6628f10891a25153b6
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 14:38:39 -0500

    Create lower_case_name.js

---
+++ lower_case_name.js
@@ -0,0 +1,8 @@
+function getLowerCaseName(name){
+    return name.toLowerCase();
+}
+
+// Sample usage (do not modify)
+console.log(getLowerCaseName("SAM"))
+console.log(getLowerCaseName("ALEX"))
+console.log(getLowerCaseName("JoHNnY"))
