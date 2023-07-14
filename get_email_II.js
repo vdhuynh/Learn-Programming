4c2873bc8cdf521a055eddb1da3e74db835bd62c
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 17:30:59 -0500

    Create get_email_II.js

---
+++ get_email_II.js
@@ -0,0 +1,8 @@
+function getEmail(email) {
+    return email.trim();
+}
+
+// Sample usage - do not modify
+console.log(getEmail("  alex@gmail.com  "))
+console.log(getEmail("  alex@gmail.com"))
+console.log(getEmail("bron@gmail.com   "))
