1b6f4763eb88226515a6d0570980f8863dfbd70b
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 14:47:06 -0500

    Create can_vote.js

---
+++ can_vote.js
@@ -0,0 +1,8 @@
+function canVote(age) {
+    return age >= 18;
+}
+
+// Sample usage (do not modify)
+console.log(canVote(10)) // should return false
+console.log(canVote(18)) // should return true
+console.log(canVote(25)) // should return true
