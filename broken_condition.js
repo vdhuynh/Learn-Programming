e43d8b233a59163700e7a80ecd7e13820eaf2e8a
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 15:16:11 -0500

    Create broken_condition.js

---
+++ broken_condition.js
@@ -0,0 +1,10 @@
+function getVotingMessage(age) {
+    if (age >= 18) {
+        return "You are allowed to vote"
+    }
+}
+
+//Sample usage - do not modify
+console.log(getVotingMessage(10))
+console.log(getVotingMessage(20))
+console.log("---")
