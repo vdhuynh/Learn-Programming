96d7f203615527e3b2980141e0d6830f62fb8f7c
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 17:35:53 -0500

    Create get_name_III.js

---
+++ get_name_III.js
@@ -0,0 +1,9 @@
+function getName(name) {
+    name = name.toUpperCase()
+    name = name.trim();
+    return name;
+}
+
+// Sample usage - do not modify
+console.log(getName(" sam"))
+console.log(getName(" alex  "))
