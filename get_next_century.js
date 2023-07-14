74f3c0ac5db4a87a32c0687de47b3279e01a5a7e
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 12:54:36 -0500

    Create get_next_century.js

---
+++ get_next_century.js
@@ -0,0 +1,8 @@
+function getNextCentury(year) {
+    console.log(year + 100);
+    return year + 100
+}
+
+// Sample usage - Do not modify (will be explained in the next lesson)
+getNextCentury(2020)
+getNextCentury(3000)
