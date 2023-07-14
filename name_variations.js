957790040b937544d16ed0e8054e5d8795eacbb1
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 14:39:59 -0500

    Create name_variations.js

---
+++ name_variations.js
@@ -0,0 +1,20 @@
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    // return the number of characters in: name
+    return name.length;
+}
+
+
+/** @param {string} name */
+export function getLower(name) {
+    // return name all in lower case (example: "ABC" becomes "abc")
+    return name.toLowerCase();
+
+}
+
+/** @param {string} name */
+export function getUpper(name) {
+    // return name all in upper case (example: "abc" becomes "ABC")
+    return name.toUpperCase();
+
+}
