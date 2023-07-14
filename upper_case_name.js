7ccca3c09137fa16e388a0c09159f40b1c4b6110
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-07-14 14:38:04 -0500

    Create upper_case_name.js

---
+++ upper_case_name.js
@@ -0,0 +1,8 @@
+function getUpperCaseName(name){
+    return name.toUpperCase();
+}
+
+// Sample usage (do not modify)
+console.log(getUpperCaseName("sam"))
+console.log(getUpperCaseName("alex"))
+console.log(getUpperCaseName("Johnny"))
